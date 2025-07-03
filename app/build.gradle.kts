a47137e0b3bf157d941e56524f5edde6d37231fe
Joedog37 <thunderheadpictures@gmail.com>
2025-07-02 22:56:59 -0400

    Refactor: Correct 'Unoffical' to 'Unofficial'
                - Updated app_name & project name
                - Corrected theme name & AndroidManifest references
                - Renamed package structure, updated namespace/applicationId
                - Resolved theme not found build error
                - Confirmed light/dark mode switching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.unofficialsatisfactorymanuel"
         minSdk = 34
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0-alpha.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
